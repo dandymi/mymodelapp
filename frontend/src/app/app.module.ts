414fd3ff2bd302c98d2853190245421b45be7f9c
Daniele Maraschi <daniele.maraschi@arcaspark.com>
2018-08-20 23:32:12 +0200

    Fix compilation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app.routing.module';
 import {UserService} from './user/user.service';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from '@angular/common/http';
 import {AddUserComponent} from './user/add-user.component';
 
 @NgModule({
